ff040608a057562ff993c7728b05b683b45c5754
Caio Mori <caiomori22@gmail.com>
2024-11-21 22:02:10 -0300

    feat: add navigation

---
+++ iQuiz/QuestionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionViewController.swift
+//  iQuiz
+//
+//  Created by Caio Mori on 21/11/24.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+        }
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        layoutConfig()
     }
-
-
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        print("Start Button Pressed")
+    }
+    
+    func layoutConfig() {
+        startButton.layer.cornerRadius = 1.0
+    }
 }
 
