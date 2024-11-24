634ddfcba13b36002199ab570c7b537f3e0730bb
Caio Mori <caiomori22@gmail.com>
2024-11-23 21:07:26 -0300

    feat: update button styles and improve question generation logic

--- iQuiz/QuestionViewController.swift
+++ iQuiz/QuestionViewController.swift
@@ -29,11 +29,16 @@ class QuestaoViewController: UIViewController {
         let isCorrectAwnser: Bool = questions[questionNumber].correctAwnser == sender.tag
         if (isCorrectAwnser) {
             score += 1
+            sender.backgroundColor = .green
+            print("O usuário acertou")
             if (questionNumber < questions.count - 1){
                 questionNumber += 1
-                generateQuestion()
+                Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(generateQuestion), userInfo: nil, repeats: false)
             }
+            return // garante que saia da função
         }
+        sender.backgroundColor = .red
+        print("O usuário errou")
     }
     
     func setLayout() {
@@ -42,16 +47,20 @@ class QuestaoViewController: UIViewController {
         questionTitleLabel.textAlignment = .center
         for botao in botoesResposta {
             botao.layer.cornerRadius = 12.0
-            botao.titleLabel?.numberOfLines = 0
+            botao.titleLabel?.numberOfLines = 1
             botao.titleLabel?.adjustsFontSizeToFitWidth = true
         }
     }
     
-    func generateQuestion(){
+    @objc func generateQuestion(){
+        if (questionNumber == questions.count - 1) {
+            questionNumber = 0
+        }
         questionTitleLabel.text = questions[questionNumber].title
         for button in botoesResposta {
             let buttonTitle = questions[questionNumber].awnsers[button.tag]
             button.setTitle(buttonTitle, for: .normal)
+            button.backgroundColor = UIColor(red: 255/255, green: 140/255, blue: 14/255, alpha: 1.0)
         }
     }
 
